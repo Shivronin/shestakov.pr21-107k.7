f2bb4e18238c68a0abf730bd837cda5e45609d24
gloufor <94900586+gloufor@users.noreply.github.com>
2022-03-24 15:03:15 +0700

    Create project

---
+++ app/src/main/java/com/example/shestakov_pr_21107k7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shestakov_pr_21107k7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
